53a572ffaa3f4ca47b1f22454237b4d709a27ac3
Vadim Godunko <vgodunko@gmail.com>
2024-10-22 13:24:41 +0400

    Update after package renaming.

--- pca9685_ps2c_servo/source/blackpill_stm32f401/pca9685_ps2c_servo-driver.adb
+++ pca9685_ps2c_servo/source/blackpill_stm32f401/pca9685_ps2c_servo-driver.adb
@@ -6,8 +6,8 @@
 
 --  Blackpill STM32F401
 
-with A0B.ARMv7M.CMSIS;
-with A0B.ARMv7M.SysTick;
+with A0B.ARMv7M.Instructions;
+with A0B.ARMv7M.SysTick_Clock;
 with A0B.STM32F401.TIM11_Timer;
 
 with PCA9685_PS2C_Servo.Configuration;
@@ -15,7 +15,7 @@ with PCA9685_PS2C_Servo.Demo;
 
 procedure PCA9685_PS2C_Servo.Driver is
 begin
-   A0B.ARMv7M.SysTick.Initialize
+   A0B.ARMv7M.SysTick_Clock.Initialize
     (Use_Processor_Clock => True,
      Clock_Frequency     => 84_000_000);
    A0B.STM32F401.TIM11_Timer.Initialize
@@ -26,6 +26,6 @@ begin
    PCA9685_PS2C_Servo.Demo.Run;
 
    loop
-      A0B.ARMv7M.CMSIS.Wait_For_Interrupt;
+      A0B.ARMv7M.Instructions.Wait_For_Interrupt;
    end loop;
 end PCA9685_PS2C_Servo.Driver;
