16822fb58a60a1a8b52b6672fd3a2d8126fe7d9a
Vadim Godunko <vgodunko@gmail.com>
2024-10-22 13:29:34 +0400

    Update after package renaming.

--- mpu9250_calibration/source/blackpill_stm32f401/mpu9250_calibration-driver.adb
+++ mpu9250_calibration/source/blackpill_stm32f401/mpu9250_calibration-driver.adb
@@ -4,7 +4,7 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
-with A0B.ARMv7M.SysTick;
+with A0B.ARMv7M.SysTick_Clock;
 with A0B.STM32F401.TIM11_Timer;
 
 with MPU9250_Calibration.Configuration;
@@ -12,7 +12,7 @@ with MPU9250_Calibration.UI;
 
 procedure MPU9250_Calibration.Driver is
 begin
-   A0B.ARMv7M.SysTick.Initialize
+   A0B.ARMv7M.SysTick_Clock.Initialize
     (Use_Processor_Clock => True,
      Clock_Frequency     => 84_000_000);
    A0B.STM32F401.TIM11_Timer.Initialize
